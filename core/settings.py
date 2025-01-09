9da0980475939c76d11f8fea33af0822ed4bd36b
Rhainer <126873901+Rhainer13@users.noreply.github.com>
2025-01-09 17:15:52 +0800

    update

--- core/settings.py
+++ core/settings.py
@@ -11,6 +11,13 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from environ import Env
+
+env = Env()
+Env.read_env()
+
+ENVIRONMENT=env('ENVIRONMENT', default='production')
+ENVIRONMENT='production'
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +27,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ht$r2s2gnw1oa%fec(75h12ej@53bwk!-((d&$5&!j5m%ryei!'
+# SECRET_KEY = 'django-insecure-ht$r2s2gnw1oa%fec(75h12ej@53bwk!-((d&$5&!j5m%ryei!'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+if ENVIRONMENT == 'development':
+    DEBUG=True
+else:
+    DEBUG=False
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '*']
 
 
 # Application definition
@@ -47,7 +60,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
-    
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
